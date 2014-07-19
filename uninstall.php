b44ce5b49386f780880348d6b0f586f9806e66d9
averta <averta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-19 14:48:28 +0000

    Tagging version 1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/master-slider/tags/1.0.7@951586 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -16,7 +16,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit( 'No Naughty Business Please !' );
 }
 
-// To uninstall the plugin completely you need to define MS_UNINSTALL_PLUGIN constant
+// To uninstall the plugin completely you need to define MS_UNINSTALL_PLUGIN constant in wp-config.php file
 // before deleting it from plugins page
 if ( defined( 'MS_UNINSTALL_PLUGIN' ) ) {
 	
