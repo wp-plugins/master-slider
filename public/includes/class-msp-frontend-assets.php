23e9a82a5f580ee6a479529c30c8dd62f39999b8
averta <averta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 13:19:24 +0000

    "Addressed a bug which stopped the sliders in some WordPress site
    git-svn-id: https://plugins.svn.wordpress.org/master-slider/trunk@1063087 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/includes/class-msp-frontend-assets.php
+++ public/includes/class-msp-frontend-assets.php
@@ -83,10 +83,23 @@ class MSP_Frontend_Assets {
 
 		// Load Css files
 		if ( $enqueue_styles ) {
-			foreach ( $enqueue_styles as $handle => $args ){
+			foreach ( $enqueue_styles as $handle => $args )
 				wp_enqueue_style( $handle, $args['src'], $args['deps'], $args['version'] );
-			}
 		}
+
+
+		// load custom.css if the directory is writable. else use inline css fallback
+	    $inline_css = msp_get_option( 'custom_inline_style', '' );
+	    if( empty( $inline_css ) ) {
+
+	    	$custom_css_ver = msp_get_option( 'masterslider_custom_css_ver', '1.0' );
+
+	    	$uploads   = wp_upload_dir();
+			$css_file  = $uploads['baseurl'] . '/' . MSWP_SLUG . '/custom.css';
+			$css_file  = apply_filters( 'masterslider_custom_css_url', $css_file );
+
+	        wp_enqueue_style ( $this->prefix . 'custom'  , $css_file , array( $this->prefix . 'main' ), $custom_css_ver );
+	    }
 
 	}
 
@@ -99,7 +112,7 @@ class MSP_Frontend_Assets {
 	 */
 	public function get_styles() {
 
-		$enqueue_styles = array(
+		return apply_filters( 'masterslider_enqueue_styles', array(
 
 			$this->prefix . 'main' => array(
 				'src'     => $this->assets_dir . '/css/masterslider.main.css' ,
@@ -107,25 +120,7 @@ class MSP_Frontend_Assets {
 				'version' => $this->version
 			)
 		
-		);
-
-		// load custom.css if the directory is writable. else use inline css fallback
-	    $inline_css = msp_get_option( 'custom_inline_style', '' );
-
-	    if( empty( $inline_css ) ) {
-	    	$custom_css_ver = msp_get_option( 'masterslider_custom_css_ver', '1.0' );
-
-	    	$uploads   = wp_upload_dir();
-			$css_file  = $uploads['baseurl'] . '/' . MSWP_SLUG . '/custom.css';
-			$css_file  = apply_filters( 'masterslider_custom_css_url', $css_file );
-
-			// enqueue custom css file in $enqueue_styles list
-			$enqueue_styles[ $this->prefix . 'custom' ] = array(
-				'src'     => $css_file ,
-				'deps'    => array( $this->prefix . 'main' ),
-				'version' => $custom_css_ver
-			);
-	    }
+		) );
 	}
 
 
@@ -135,19 +130,15 @@ class MSP_Frontend_Assets {
 	function inline_css_fallback(){
 
 	    $inline_css = msp_get_option( 'custom_inline_style', '' );
-	    $inline_css = apply_filters( 'masterslider_custom_inline_style', $inline_css );
 	    
 	    // if custom.css is not writable, print css styles in page header
 	    if( ! empty( $inline_css ) ) {
-	    	if( current_user_can( 'manage_options' ) ){
+	    	if( current_user_can( 'manage_options' ) )
 	    		printf( "<!-- Note for admin: The custom.css file in [%s] is not writeable, so masterslider uses inline css callback instead. -->\n", MSWP_AVERTA_URL . '/assets/custom.css' );
-	    	}
 	    	printf( "<style>%s</style>\n", $inline_css );
 	    }
 
-		printf( "<script>var ms_grabbing_curosr='%s',ms_grab_curosr='%s';function msCli(f){f=f||'pause';var m=masterslider_instances;for(var i in m){m[i].api[f]();}}</script>\n", 
-	             MSWP_AVERTA_URL . '/public/assets/css/common/grabbing.cur', 
-	             MSWP_AVERTA_URL . '/public/assets/css/common/grab.cur' );
+	    printf( "<script>var ms_grabbing_curosr = '%s', ms_grab_curosr = '%s';</script>\n", MSWP_AVERTA_URL . '/public/assets/css/common/grabbing.cur', MSWP_AVERTA_URL . '/public/assets/css/common/grab.cur' );
 	}
 
 
