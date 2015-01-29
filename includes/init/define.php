4f8da0a3858e25e4d2d1ebd728c83133a766f374
averta <averta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-29 14:38:03 +0000

    Tagging version 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/master-slider/tags/2.0.2@1078228 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/init/define.php
+++ includes/init/define.php
@@ -5,7 +5,7 @@ if ( ! defined('ABSPATH') ) {
     die();
 }
 
-define( 'MSWP_AVERTA_VERSION'		, '2.0.0' );
+define( 'MSWP_AVERTA_VERSION'		, '2.0.2' );
 
 define( 'MSWP_SLUG'					, 'master-slider' );
 define( 'MSWP_TEXT_DOMAIN'			, 'masterslider' );
